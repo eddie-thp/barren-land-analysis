8bec5365eb5b7623df00c4ff812d2d43eef046ad
Eduardo Przysiezny <eduardo_thp@yahoo.com.br>
2020-04-04 00:31:40 -0500

    Implement barren land analysis using a set of coordinates that represent one area unit Use a main class to execute the sample scenarios from the technical assessment document

--- src/main/java/org/ethp/barrenland/main/BarrenLandAnalysisApp.java
+++ src/main/java/org/ethp/barrenland/main/BarrenLandAnalysisApp.java
@@ -1,30 +1,64 @@
 package org.ethp.barrenland.main;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.ethp.barrenland.main.model.Land;
 import org.ethp.barrenland.main.model.Rectangle;
 
 public class BarrenLandAnalysisApp {
-	
-    private static final int LAND_WIDTH = 400;
-    private static final int LAND_HEIGHT = 600;
+
+	private static final int LAND_WIDTH = 400;
+	private static final int LAND_HEIGHT = 600;
 
 	public static void main(String[] args) {
-        Rectangle barrenArea = new Rectangle(0, 292, 399, 307);
+		executeSample0();
+		executeSample1();
+	}
 
-        Set<Rectangle> barrentAreas = new HashSet<>();
-        barrentAreas.add(barrenArea);
+	public static void executeSample0() {
+		Rectangle barrenArea = new Rectangle(0, 292, 399, 307);
 
-        Land land = new Land(LAND_WIDTH, LAND_HEIGHT, barrentAreas);
+		Set<Rectangle> barrentAreas = new HashSet<>();
+		barrentAreas.add(barrenArea);
 
-        System.out.println("RECT W " + barrenArea.getWidth());
-        System.out.println("RECT H " + barrenArea.getHeight());
-        System.out.println("RECT Area " + barrenArea.getArea());
-        System.out.println("LAND Area " + land.getArea());
-        System.out.println("FERTILE AREA " + ((land.getArea() - barrenArea.getArea()) / 2));
+		Land land = new Land(LAND_WIDTH, LAND_HEIGHT, barrentAreas);
 
+		printFertileAreas(land);
+	}
+
+	public static void executeSample1() {
+		Rectangle barrenArea0 = new Rectangle(48, 192, 351, 207);
+		Rectangle barrenArea1 = new Rectangle(48, 392, 351, 407);
+		Rectangle barrenArea2 = new Rectangle(120, 52, 135, 547);
+		Rectangle barrenArea3 = new Rectangle(260, 52, 275, 547);
+
+		Set<Rectangle> barrentAreas = new HashSet<>();
+		barrentAreas.add(barrenArea0);
+		barrentAreas.add(barrenArea1);
+		barrentAreas.add(barrenArea2);
+		barrentAreas.add(barrenArea3);
+
+		Land land = new Land(LAND_WIDTH, LAND_HEIGHT, barrentAreas);
+
+		printFertileAreas(land);
+	}
+
+	public static void printFertileAreas(Land land) {
+
+		System.out.println("AREA: " + land.getArea());
+		System.out.println("FERTILE AREA: " + land.getTotalFertileArea());
+		System.out.println("BARREN AREA: " + land.getTotalBarrenArea());
+		System.out.print("FERTILE AREAS: ");
+
+		final List<Integer> fertileAreas = land.getFertileAreas();
+		for (Integer fertileArea : fertileAreas) {
+			System.out.print(fertileArea);
+			System.out.print(" ");
+		}
+
+		System.out.println();
 	}
 
 }
--- src/main/java/org/ethp/barrenland/main/model/Land.java
+++ src/main/java/org/ethp/barrenland/main/model/Land.java
@@ -1,12 +1,41 @@
 package org.ethp.barrenland.main.model;
 
 import java.security.InvalidParameterException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Land extends Rectangle {
-	private boolean[][] fertile;
+
+	private class LandUnit {
+		int x;
+		int y;
+
+		public LandUnit(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(Integer.valueOf(x), Integer.valueOf(y));
+		}
+
+		@Override
+		public boolean equals(Object object) {
+			if (object == null || !(object instanceof LandUnit)) {
+				return false;
+			}
+
+			LandUnit unit = (LandUnit) object;
+			return (this.x == unit.x && this.y == unit.y);
+		}
+	}
+
+	private Set<LandUnit> fertileArea;
 
 	public Land(int width, int height) throws InvalidParameterException {
 		super(0, 0, width - 1, height - 1);
@@ -19,10 +48,12 @@ public class Land extends Rectangle {
 			throw new InvalidParameterException("Height smaller or equals to 0.");
 		}
 
-		this.fertile = new boolean[width][height];
+		fertileArea = new HashSet<>();
 
 		for (int x = 0; x < width; x++) {
-			Arrays.fill(fertile[x], Boolean.TRUE);
+			for (int y = 0; y < height; y++) {
+				fertileArea.add(new LandUnit(x, y));
+			}
 		}
 	}
 
@@ -35,7 +66,7 @@ public class Land extends Rectangle {
 	}
 
 	private void setBarrenRectangle(Rectangle rectangle) throws InvalidParameterException {
-		
+
 		final int width = getWidth();
 		final int height = getHeight();
 
@@ -51,7 +82,7 @@ public class Land extends Rectangle {
 
 		for (int x = left; x <= right; x++) {
 			for (int y = bottom; y <= top; y++) {
-				this.fertile[x][y] = false;
+				fertileArea.remove(new LandUnit(x, y));
 			}
 		}
 	}
@@ -62,18 +93,7 @@ public class Land extends Rectangle {
 	 * @return total fertile area
 	 */
 	public int getTotalFertileArea() {
-		final int width = getWidth();
-		final int height = getHeight();
-
-		int fertileArea = 0;
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if (this.fertile[x][y]) {
-					fertileArea++;
-				}
-			}
-		}
-		return fertileArea;
+		return fertileArea.size();
 	}
 
 	/**
@@ -82,23 +102,69 @@ public class Land extends Rectangle {
 	 * @return total barren area
 	 */
 	public int getTotalBarrenArea() {
-		final int width = getWidth();
-		final int height = getHeight();
+		return getArea() - getTotalFertileArea();
+	}
 
-		int barrenArea = 0;
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				if (!this.fertile[x][y]) {
-					barrenArea++;
-				}
-			}
-		}
-		return barrenArea;
+	private boolean isFertile(final LandUnit unit) {
+		return fertileArea.contains(unit);
 	}
 
 	public List<Integer> getFertileAreas() {
-		// TODO bla bla
-		return null;
-	}
+		// Iterate over the fertile area units, grouping them in a set if they are
+		// connected
+		List<Set<LandUnit>> unitFertileAreas = new ArrayList<>();
 
+		// Holds the fertile area being currently processed by the loop
+		int currentFertileAreaIndex = -1;
+		Set<LandUnit> currentFertileArea = null;
+
+		final int width = getWidth();
+		final int height = getHeight();
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				LandUnit unit = new LandUnit(x, y);
+				if (isFertile(unit)) {
+					if (currentFertileArea == null) {
+						currentFertileArea = new HashSet<>();
+						unitFertileAreas.add(currentFertileArea);
+						currentFertileAreaIndex = unitFertileAreas.size() - 1;
+					}
+
+					currentFertileArea.add(unit);
+
+					if (y > 0) {
+						LandUnit unitBelow = new LandUnit(x, y - 1);
+						if (isFertile(unitBelow) && !currentFertileArea.contains(unitBelow)) {
+							// Remove the currentFertileArea, find the fertile area where the unit below is
+							// located, and merge the current onto it.
+							unitFertileAreas.remove(currentFertileAreaIndex);
+
+							for (int i = 0; i < unitFertileAreas.size(); i++) {
+								Set<LandUnit> unitFertileArea = unitFertileAreas.get(i);
+								if (unitFertileArea.contains(unitBelow)) {
+									unitFertileArea.addAll(currentFertileArea);
+									currentFertileArea = unitFertileArea;
+									currentFertileAreaIndex = i;
+									break;
+								}
+							}
+						}
+					}
+				} else {
+					currentFertileAreaIndex = -1;
+					currentFertileArea = null;
+				}
+			}
+		}
+
+		// The area amount is the size of the fertile area
+		List<Integer> fertileAreas = new ArrayList<>();
+		for (Set<LandUnit> fertileArea : unitFertileAreas) {
+			fertileAreas.add(fertileArea.size());
+		}
+
+		Collections.sort(fertileAreas);
+
+		return fertileAreas;
+	}
 }
